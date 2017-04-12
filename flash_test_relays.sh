d01536a902842224cc96f4d4430807685f7466b1
Bob Davis <bob.davis@fenome.com>
2017-04-12 23:30:44 +0000

    script to flash arduino to rotate through all relays

---
+++ flash_test_relays.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/pi/test_relays/
+# avrdude -v -F -V -D -c stk500v2 -p ATmega2560 -P /dev/ttyACM0 -b 115200 -U flash:w:sketch_off.ino.mega.hex
+avrdude -v -F -V -D -c stk500v2 -p ATmega2560 -P /dev/ttyACM0 -b 115200 -U flash:w:Brain_Buyoff.ino.mega.hex
---
+++ off.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // set all relay pins and power pins to output high
+
+  // set ground pins to output low
+
+}
+
+void loop() {
+  // run through relay pins and turn on individually for audible test
+}
